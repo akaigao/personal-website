a1a2825d28cc52f2fa18c08e4e8def874a06b0e2
Igão <igaosouza@Igaos-MacBook-Pro.local>
2022-12-23 19:40:09 -0300

    chore: developer configs

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`;
+`
 
 export default function Home() {
   return (
     <>
       <Title>Welcome to my homepage w/ Next.JS!</Title>
     </>
-  );
+  )
 }
